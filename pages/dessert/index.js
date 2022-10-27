749c7b421a4db3390c59015128ca17c582c48423
Gai Isay <gaiisay@outlook.de>
2022-10-27 11:40:36 +0200

    add two pages and some dummy content

---
+++ pages/dessert/index.js
@@ -0,0 +1,5 @@
+function Dessert() {
+  return <h1>This is the Dessert page</h1>;
+}
+
+export default Dessert;
---
+++ pages/veggie/index.js
@@ -0,0 +1,5 @@
+function Veggie() {
+  return <h1>This is the Veggie page</h1>;
+}
+
+export default Veggie;
