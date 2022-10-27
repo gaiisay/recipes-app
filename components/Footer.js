97fab1a5c036d3edd87e3da355cb2c0fe965474b
Gai Isay <gaiisay@outlook.de>
2022-10-27 11:53:56 +0200

    create layout

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>This is my footer</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>Recipes</h1>
+      <nav>
+        <a href="/">home</a>
+        <a href="/veggie">veggie</a>
+        <a href="/dessert">dessert</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
