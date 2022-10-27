5448cc09599ce45588d9f622b4cda44a611b9fbf
Gai Isay <gaiisay@outlook.de>
2022-10-27 12:10:42 +0200

    add image

--- pages/veggie/index.js
+++ pages/veggie/index.js
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import pizza from "../../public/pizza.jpg";
+
 function Veggie() {
-  return <h1>This is the Veggie page</h1>;
+  return (
+    <>
+      <h1>This is the Veggie page</h1>
+      <Image src={pizza} alt="pizza image" layout="responsive" width={500} placeholder="blur" />
+    </>
+  );
 }
 
 export default Veggie;
