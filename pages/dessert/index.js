91725738c25b071aa19fcfddf301d1ea35bba76c
Gai Isay <gaiisay@outlook.de>
2022-10-27 12:31:11 +0200

    add remote image

--- pages/dessert/index.js
+++ pages/dessert/index.js
@@ -1,5 +1,18 @@
+import Image from "next/image";
+
 function Dessert() {
-  return <h1>This is the Dessert page</h1>;
+  return (
+    <>
+      <h1>This is the Dessert page</h1>
+      <Image
+        src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400"
+        alt="pizza image"
+        layout="responsive"
+        width={500}
+        height={500}
+      />
+    </>
+  );
 }
 
 export default Dessert;
